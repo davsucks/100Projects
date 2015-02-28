a839ccff3d1bdae660f0cd363ec0ddde423b1c31
David <davsucks@umich.edu>
2015-02-28 18:26:34 -0500

    Add test for assign_change_hash

--- Numbers/change_return/change_return_test_suite.rb
+++ Numbers/change_return/change_return_test_suite.rb
@@ -18,60 +18,20 @@ RSpec.describe ChangeReturn do
 		end
 	end
 
-	describe '.run' do
-		it 'returns .calculate_change' do
-			expect(ChangeReturn).to receive(:calculate_change).and_return( {} )
-			expect(ChangeReturn).to receive(:prompt_user).twice
-			expect(ChangeReturn).to receive(:display_change).and_return("ret")
-			expect(ChangeReturn.run).to eq("ret")
-		end
-	end
-
-	describe '.check_input' do
-		context 'when given incorrect input' do
-			it 'raises an exception' do
-				expect{ ChangeReturn.check_input(nil) }.to raise_error("Bad user input")
-			end
-		end
-
-		context 'when given correct input' do
-			it 'does nothing' do
-				expect{ ChangeReturn.check_input(12.43) }.to_not raise_error
-			end
-		end
-	end
-
-	describe '.proper_pluralize' do
-		context 'when passing in one item' do
-			it 'returns the original word' do
-				expect(ChangeReturn.proper_pluralize(1, 'penny')).to eq('penny')
-			end
-		end
-
-		context 'when passing in more than one item' do
-			context 'when the word does not end in y' do
-				it 'adds "s" to the original word' do
-					expect(ChangeReturn.proper_pluralize(2, 'quarter')).to eq('quarters')
-				end
-			end
-
-			context 'when the word ends in "y"' do
-				it 'replaces "y" with "ies"' do
-					expect(ChangeReturn.proper_pluralize(2, 'penny')).to eq('pennies')
-				end
-			end
-		end
-	end
-
-	describe '.display_change' do
-		it 'returns a nicely displayed hash' do
-			expect(ChangeReturn).to receive(:puts)
-			expect(ChangeReturn).to receive(:proper_pluralize).exactly(5).times
-			ChangeReturn.display_change({})
-		end
-	end
-
 	describe '.calculate_change' do
+		it 'returns assign_change_hash' do
+			expect(ChangeReturn).to receive(:assign_change_hash).and_return('ret')
+			expect(ChangeReturn.calculate_change(2, 3)).to eq('ret')
+		end
+
+		context 'when not given enough money' do
+			it 'raises an error' do
+				expect{ ChangeReturn.calculate_change(3, 2) }.to raise_error("Not enough money")
+			end
+		end
+	end
+
+	describe '.assign_change_hash' do
 		context 'when the cost is 5 dollars' do
 			context 'when given 5 dollars' do
 				it 'returns no change' do
@@ -127,11 +87,60 @@ RSpec.describe ChangeReturn do
 				end
 			end
 		end
+	end
 
-		context 'when not given enough money' do
-			it 'raises an error' do
-				expect{ ChangeReturn.calculate_change(3, 2) }.to raise_error("Not enough money")
+	describe '.run' do
+		it 'returns .calculate_change' do
+			expect(ChangeReturn).to receive(:calculate_change).and_return( {} )
+			expect(ChangeReturn).to receive(:prompt_user).twice
+			expect(ChangeReturn).to receive(:display_change).and_return("ret")
+			expect(ChangeReturn.run).to eq("ret")
+		end
+	end
+
+	describe '.check_input' do
+		context 'when given incorrect input' do
+			it 'raises an exception' do
+				expect{ ChangeReturn.check_input(nil) }.to raise_error("Bad user input")
+			end
+		end
+
+		context 'when given correct input' do
+			it 'does nothing' do
+				expect{ ChangeReturn.check_input(12.43) }.to_not raise_error
 			end
 		end
 	end
+
+	describe '.proper_pluralize' do
+		context 'when passing in one item' do
+			it 'returns the original word' do
+				expect(ChangeReturn.proper_pluralize(1, 'penny')).to eq('penny')
+			end
+		end
+
+		context 'when passing in more than one item' do
+			context 'when the word does not end in y' do
+				it 'adds "s" to the original word' do
+					expect(ChangeReturn.proper_pluralize(2, 'quarter')).to eq('quarters')
+				end
+			end
+
+			context 'when the word ends in "y"' do
+				it 'replaces "y" with "ies"' do
+					expect(ChangeReturn.proper_pluralize(2, 'penny')).to eq('pennies')
+				end
+			end
+		end
+	end
+
+	describe '.display_change' do
+		it 'returns a nicely displayed hash' do
+			expect(ChangeReturn).to receive(:puts)
+			expect(ChangeReturn).to receive(:proper_pluralize).exactly(5).times
+			ChangeReturn.display_change({})
+		end
+	end
+
+	
 end
