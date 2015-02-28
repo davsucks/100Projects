70e077c876f20bebed3456df51a9c44650ac14ba
David <davsucks@umich.edu>
2015-02-28 17:45:55 -0500

    Add error check to calculate_change

--- Numbers/change_return/change_return_ruby.rb
+++ Numbers/change_return/change_return_ruby.rb
@@ -12,17 +12,17 @@ class ChangeReturn
 	end
 
 	# returns a hash of each type of US coin and the amount of that coin
-	def self.calculate_change(cost, amount_give)
-
+	def self.calculate_change(cost, amount_given)
+		raise "Not enough money" if cost > amount_given
 	end
 
 	def self.display_change(change_hash)
 		puts "Your change is $#{ change_hash[:total] }:\n
-					\t#{ change_hash[:dollars] } dollar#{ change_hash[:dollars] > 1 ? 's' : ''},\n
-					\t#{ change_hash[:quarters] } quarter#{ change_hash[:quarters] > 1 ? 's' : ''},\n
-					\t#{ change_hash[:dimes] } dime#{ change_hash[:dimes] > 1 ? 's' : ''},\n
-					\t#{ change_hash[:nickels] } nickel#{ change_hash[:nickels] > 1 ? 's' : ''},\n
-					\t#{ change_hash[:pennies] } penn#{ change_hash[:pennies] > 1 ? 'ies' : 'y'}"
+					\t#{ change_hash[:dollars] } #{ proper_pluralize(change_hash[:dollars], 'dollar') },\n
+					\t#{ change_hash[:quarters] } #{ proper_pluralize(change_hash[:quarters], 'quarter') },\n
+					\t#{ change_hash[:dimes] } #{ proper_pluralize(change_hash[:dimes], 'dime') },\n
+					\t#{ change_hash[:nickels] } #{ proper_pluralize(change_hash[:nickels], 'nickel') },\n
+					\t#{ change_hash[:pennies] } #{ proper_pluralize(change_hash[:pennies], 'penny') }"
 	end
 
 	def self.proper_pluralize(num_amount, word)
@@ -40,10 +40,10 @@ class ChangeReturn
 
 	def self.run
 		prompt_user("Please input the cost:")
-		cost = get_user_input.to_f
+		cost = check_input(get_user_input.to_f)
 
 		prompt_user("Please input the total given:")
-		amount_given = get_user_input.to_f
+		amount_given = check_input(get_user_input.to_f)
 
 		display_change(calculate_change(cost, amount_given))
 	end
