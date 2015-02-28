9d88050ac186c5ab160ee84299f7610be8273e90
David <davsucks@umich.edu>
2015-02-28 17:25:08 -0500

    More tests and body

--- Numbers/change_return/change_return_ruby.rb
+++ Numbers/change_return/change_return_ruby.rb
@@ -7,8 +7,44 @@ class ChangeReturn
 		gets.chomp
 	end
 
+	def self.prompt_user(msg)
+		puts msg
+	end
+
+	# returns a hash of each type of US coin and the amount of that coin
+	def self.calculate_change(cost, amount_give)
+
+	end
+
+	def self.display_change(change_hash)
+		puts "Your change is $#{ change_hash[:total] }:\n
+					\t#{ change_hash[:dollars] } dollar#{ change_hash[:dollars] > 1 ? 's' : ''},\n
+					\t#{ change_hash[:quarters] } quarter#{ change_hash[:quarters] > 1 ? 's' : ''},\n
+					\t#{ change_hash[:dimes] } dime#{ change_hash[:dimes] > 1 ? 's' : ''},\n
+					\t#{ change_hash[:nickels] } nickel#{ change_hash[:nickels] > 1 ? 's' : ''},\n
+					\t#{ change_hash[:pennies] } penn#{ change_hash[:pennies] > 1 ? 'ies' : 'y'}"
+	end
+
+	def self.proper_pluralize(num_amount, word)
+		if num_amount == 1
+			return word
+		else 
+			return word.chop + 'ies' if (word[-1] == 'y')
+			return word + 's'
+		end
+	end
+
+	def self.check_input(input)
+		raise "Bad user input" if input.nil?
+	end
+
 	def self.run
-		cost = get_user_input
-		amount_given = get_user_input
+		prompt_user("Please input the cost:")
+		cost = get_user_input.to_f
+
+		prompt_user("Please input the total given:")
+		amount_given = get_user_input.to_f
+
+		display_change(calculate_change(cost, amount_given))
 	end
 end
--- Numbers/change_return/change_return_test_suite.rb
+++ Numbers/change_return/change_return_test_suite.rb
@@ -11,9 +11,78 @@ RSpec.describe ChangeReturn do
 		end
 	end
 
-	describe '.run' do
-		it 'returns something' do
+	describe '.prompt_user' do
+		it 'outputs the message passed in' do
+			expect(ChangeReturn).to receive(:puts).with("user_input")
+			ChangeReturn.prompt_user("user_input")
+		end
+	end
 
+	describe '.run' do
+		it 'returns .calculate_change' do
+			expect(ChangeReturn).to receive(:calculate_change).and_return( {} )
+			expect(ChangeReturn).to receive(:prompt_user).twice
+			expect(ChangeReturn).to receive(:display_change).and_return("ret")
+			expect(ChangeReturn.run).to eq("ret")
+		end
+	end
+
+	describe '.check_input' do
+		context 'when given incorrect input' do
+			it 'raises an exception' do
+				expect{ ChangeReturn.check_input(nil) }.to raise_error("Bad user input")
+			end
+		end
+
+		context 'when given correct input' do
+			it 'does nothing' do
+				expect{ ChangeReturn.check_input(12.43) }.to_not raise_error
+			end
+		end
+	end
+
+	describe '.proper_pluralize' do
+		context 'when passing in one item' do
+			it 'returns the original word' do
+				expect(ChangeReturn.proper_pluralize(1, 'penny')).to eq('penny')
+			end
+		end
+
+		context 'when passing in more than one item' do
+			context 'when the word does not end in y' do
+				it 'adds "s" to the original word' do
+					expect(ChangeReturn.proper_pluralize(2, 'quarter')).to eq('quarters')
+				end
+			end
+
+			context 'when the word ends in "y"' do
+				it 'replaces "y" with "ies"' do
+					expect(ChangeReturn.proper_pluralize(2, 'penny')).to eq('pennies')
+				end
+			end
+		end
+	end
+
+	describe '.display_change' do
+		it 'returns a nicely displayed hash' do
+			change_hash = {
+				total: 5.41,
+				dollars: 5,
+				quarters: 1,
+				dimes: 1,
+				nickels: 1,
+				pennies: 1
+			}
+		end
+	end
+
+	describe '.calculate_change' do
+		context 'when the cost is 5 dollars' do
+			context 'when given 5 dollars' do
+				it 'returns no change' do
+					expect(ChangeReturn.calculate_change(5, 5)).to eq(0)
+				end
+			end
 		end
 	end
 end
