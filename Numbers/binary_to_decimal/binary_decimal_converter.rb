72a536af756bd07eb83d132b374f2e932a124ce2
David <davsucks@umich.edu>
2015-03-01 12:09:21 -0500

    Finish binary to decimal impl

--- Numbers/binary_to_decimal/binary_decimal_converter.rb
+++ Numbers/binary_to_decimal/binary_decimal_converter.rb
@@ -1,3 +1,21 @@
 # **Binary to Decimal and Back Converter**
 # Develop a converter to convert a decimal number to binary or a
 # binary number to its decimal equivalent.
+
+class BinaryDecimal
+	def self.binary_to_decimal(binary)
+		calculate_decimal_from_binary(binary.to_s)
+	end
+
+	def self.calculate_decimal_from_binary(binary)
+		sum = 0
+		binary.chars.reverse.each_with_index do |bit, index|
+			sum += 2**index if bit == '1'
+		end
+		sum
+	end
+
+	def self.decimal_to_binary(decimal)
+
+	end
+end
